fc51124feac0884a043a164262fc1ce3df071735
Andrey Fits <andreyfits@gmail.com>
2022-05-09 21:25:48 +0300

    feat: configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+let mode = 'development';
+
+if (process.env.NODE_ENV === 'production') {
+    mode = 'production';
+}
+
+module.exports = {
+    mode: mode,
+    entry: './src/index.js',
+    module: {
+        rules: [
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    (mode === 'development') ? 'style-loader' : MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+        ],
+    },
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: '[name].[contenthash].js',
+        assetModuleFilename: 'assets/[hash][ext][query]',
+        clean: true,
+    },
+    devServer: {
+        open: true,
+        static: {
+            directory: './src',
+            watch: true,
+        },
+    },
+    devtool: 'source-map',
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            inject: 'body',
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'style.[contenthash].css',
+        }),
+    ],
+};
